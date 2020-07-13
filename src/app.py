fffa565c54fc785a38188c71dc57eed89daec693
filipStar <file.starcevic@gmail.com>
2020-07-13 11:17:54 +0200

    Added styling to streamlit app

--- src/app.py
+++ src/app.py
@@ -12,32 +12,53 @@ def main():
     cves = cves_large.loc[~cves_large['cve_id'].duplicated(keep='first')]
 
     st.title('Welcome to the Fidelio Visualizer')
-
-    st.write(cves['published_date'].dt.date.sort_values().unique())
+    st.write('A Streamlit Application used for the visualization of Common Vulnerabilities and Exposures.')
+    #st.write(cves.head())
+    draw_graph6(cves)
+    draw_graph1(cves)
+    draw_graph4(cves)
+    draw_graph3(cves)
     draw_graph2(cves_large)
+    draw_graph5(cves_large)
+
 
 def draw_graph6(data):
     cves = data
+    select = st.sidebar.selectbox('Time options', ['Last week', 'Last 30 days', 'Last 90 days', 'Last year', 'All time'])
 
     dates = cves['published_date'].dt.date.sort_values().unique()
 
-    from_date = st.sidebar.date_input("From", min_value=dates[1],
+    if select == 'Last week':
+        from_value = dates[-1] - dt.timedelta(days=6)
+        to_value = dates[-1]
+    elif select == 'Last 30 days':
+        from_value = dates[-1] - dt.timedelta(days=30)
+        to_value = dates[-1]
+    elif select == 'Last 90 days':
+        from_value = dates[-1] - dt.timedelta(days=90)
+        to_value = dates[-1]
+    elif select == 'Last year':
+        from_value = dates[-1] - dt.timedelta(days=365)
+        to_value = dates[-1]
+    elif select == 'All time':
+        from_value = dates[1]
+        to_value = dates[-1]
+
+    date = st.sidebar.date_input("From", min_value=dates[1],
                                     max_value=dates[-2],
-                                    value=dates[-1]-dt.timedelta(days=30))
+                                    value=[from_value, to_value])
 
-    to_date = st.sidebar.date_input("To", min_value=dates[2],
-                                    max_value=dates[-1],
-                                    value=dates[-1])
-    
-    fig = go.Figure(data=go.Scatter(y=cves['published_date'].dt.date.value_counts().sort_index().loc[from_date:to_date],
-                                    x=cves['published_date'].dt.date.value_counts().sort_index().loc[from_date:to_date].index
-                                    ))
-    fig.update_layout(title='Number of CVEs by Time',
-                   xaxis_title='Date',
-                   yaxis_title='Number of CVEs')
-
-    st.plotly_chart(fig)
-
+    try:
+        fig = go.Figure(data=go.Scatter(y=cves['published_date'].dt.date.value_counts().sort_index().loc[date[0]:date[1]],
+                                        x=cves['published_date'].dt.date.value_counts().sort_index().loc[date[0]:date[1]].index
+                                        ))
+        fig.update_layout(title='Number of CVEs by Time',
+                            xaxis_title='Date',
+                            yaxis_title='Number of CVEs')
+        fig.update_traces(hoverinfo='x+y', hovertemplate='%{x} <extra>%{y}</extra>')
+        st.plotly_chart(fig)
+    except IndexError:
+        st.error('Please select a date range.')
 
 def draw_graph5(data):
     cves = data
@@ -51,15 +72,16 @@ def draw_graph5(data):
 
     fig = px.bar(products.iloc[:display], x='product',
                 labels={'product': 'Number of CVEs', 'index': 'Product'}, text='product')
-    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
-    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside',
+                    hovertemplate='%{x} <extra></extra>')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', title='Total Number of Vulnerabilities by Product')
     st.plotly_chart(fig)
 
 
 def draw_graph4(data):
     cves = data
 
-    display = select_option()
+    display, select = select_option('graph4')
     first_value = [cves[f'{display}'].value_counts().iloc[0]]
     second_value = [cves[f'{display}'].value_counts().iloc[1]]
     third_value  = [cves[f'{display}'].value_counts().iloc[2]]
@@ -70,8 +92,9 @@ def draw_graph4(data):
     go.Bar(name=cves[f'{display}'].value_counts().index[1], y=second_value, hoverinfo='y+name', text=second_value),
     go.Bar(name=cves[f'{display}'].value_counts().index[2], y=third_value, hoverinfo='y+name', text=third_value)
     ])
-    fig.update_layout(barmode='group', height=475)
-    fig.update_traces(textposition='outside')
+    fig.update_layout(barmode='group', height=475, title=f'Total number of Vulnerabilities by {select}')
+    fig.update_traces(textposition='outside', hoverinfo='y+name')
+    fig.update_xaxes(showticklabels=False)
     st.plotly_chart(fig)
 
 def draw_graph3(data):
@@ -93,7 +116,10 @@ def draw_graph3(data):
         scores = scores.append({'range': f'{i}-{j}', 'score': cves[f'{score}'].loc[filt].count()}, ignore_index=True)
 
     fig = px.bar(scores, x='range', y='score', color='range',
-                labels={'range': select, 'score': 'Number of CVEs'})
+                labels={'range': select, 'score': 'Number of CVEs'}, 
+                title=f'Total number of Vulnerabilities by {select}')
+    
+    fig.update_traces(texttemplate='%{y}', textposition='outside')
     st.plotly_chart(fig)
 
 
@@ -109,15 +135,16 @@ def draw_graph2(data):
 
     fig = px.bar(vendors.iloc[:display], x='vendor',
                 labels={'vendor': 'Number of CVEs', 'index': 'Vendor'}, text='vendor', )
-    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
-    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside',
+                    hovertemplate='%{x} <extra></extra>')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', title='Total Number of Vulnerabilities by Vendor')
     st.plotly_chart(fig)
 
 
 def draw_graph1(data):
     cves = data
 
-    display = select_option()
+    display, select = select_option('graph1')
     years = cves['published_date'].dt.year.unique()
     first_list = []
     second_list = []
@@ -147,16 +174,17 @@ def draw_graph1(data):
     go.Bar(name=cves[f'{display}'].value_counts().sort_index(ascending=False).index[1], x=years, y=second_list, text=second_list),
     go.Bar(name=cves[f'{display}'].value_counts().sort_index(ascending=False).index[2], x=years, y=third_list, text=third_list)
     ])
-    fig.update_layout(barmode='group', height=475)
+    fig.update_layout(barmode='group', height=475, title=f'Total number of Vulnerabilities by {select} by year',
+                    uniformtext_minsize=8, uniformtext_mode='hide')
     fig.update_traces(textposition='outside', hoverinfo='y+name')
     st.plotly_chart(fig)
 
 
-def select_option():
+def select_option(key):
     select = st.selectbox('Select what to display', ['Severity',
                         'Access Complexity', 'Access Vector',
                         'Access Authentication', 'Confidentiality Impact',
-                        'Integrity Impact', 'Availability Impact'])
+                        'Integrity Impact', 'Availability Impact'], key=key)
     if select == 'Severity':
         display = 'cvss_severity'
     elif select == 'Access Complexity':
@@ -172,7 +200,7 @@ def select_option():
     elif select == 'Availability Impact':
         display = 'cvss_availability_impact'
 
-    return display
+    return display, select
 
 @st.cache
 def get_data():
